09949f6b302043464271544a0f1bb1d7125935aa
Nathalie Guibert <91286684+miaunala@users.noreply.github.com>
2025-01-20 22:46:57 +0100

    Update api key and file structure to avoid exposure

--- annotation_gpt/gpt_annotation_task_v1_4_3.5turbo.py
+++ annotation_gpt/gpt_annotation_task_v1_4_3.5turbo.py
@@ -5,10 +5,10 @@ import tiktoken
 
 
 # Paths to datasets and codebook
-base = "C:/Users/whatt/iCloudDrive/UZH/Master/Master Thesis/gpt_hatestoxicspeech/"
+base = "C:/Users/.../gpt_hatestoxicspeech/"
 covid_path = base + "sample_covid.csv"
 farmer_path = base + "sample_farmer.csv"
-openai.api_key = "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"
+openai.api_key = "YOUR_API_KEY"
 
 # Covid Stuff
 covid_msg = pd.read_csv(covid_path, encoding='utf-8')
